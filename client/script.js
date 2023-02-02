b524cf88bd5d3e3a0a47e1b4beac2bac5db565e4
mertzdog <mertzdog@gmail.com>
2023-02-01 20:38:54 -0500

    update the server url

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
   showLoadingIndicator(messageDiv);
 
   // Fetch the bot's response from the server
-  const response = await fetch('http://localhost:5002', {
+  const response = await fetch('https://elots.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
